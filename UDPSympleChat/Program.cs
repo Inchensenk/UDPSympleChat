6e44d03e8730ea9ce45af12d45206a35eb87d64c
Inchensenk <aab8117@gmail.com>
2022-10-10 17:19:50 +0300

    Добавьте файлы проекта.

---
+++ UDPSympleChat/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace UDPSympleChat
+{
+
+    class Program
+    {
+        static int localPort;
+        static int remotePort;
+        static Socket listeningSocket;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите порт для приема сообщений: ");
+            localPort = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Введите порт ");
+        }
+    }
+}
